18a63ba15175680b8b1ea73104734647b2b9eef1
Nursima Asilturk <nursima.asilturk99@gmail.com>
2024-12-12 22:43:06 +0300

    sorting box classNames aremade is simple

--- src/Components/Sorting.js
+++ src/Components/Sorting.js
@@ -9,11 +9,11 @@ function Sorting() {
 	    <button onClick={toggleClass} className='w-[24px]'>
 			<img src={sortImg} alt="sort" className='w-full h-auto' />
 		</button>
-		<ul className={`${isOpen ? "visible" :"invisible"} bg-white p-2 absolute z-[2] min-w-[200px] right-0 rounded-lg top-[40px]`}>
-			<li className='w-full '><button className='text-secondary font-light p-1 hover:pl-2 w-full text-left hover:bg-secondary hover:text-white rounded-[5px]'>A to Z</button></li>
-			<li  className='w-full '><button className='text-secondary font-light p-1 hover:pl-2  w-full text-left hover:bg-secondary hover:text-white rounded-[5px]'>Z to A</button></li>
-			<li className='w-full '><button className='text-secondary font-light p-1 hover:pl-2  w-full text-left hover:bg-secondary hover:text-white rounded-[5px]'>Newest Contact</button></li>
-			<li  className='w-full '><button className='text-secondary font-light p-1 hover:pl-2  w-full text-left hover:bg-secondary hover:text-white rounded-[5px]'>Oldest Contact</button></li>
+		<ul className={`${isOpen ? "visible" :"invisible"} sorting-box`}>
+			<li><button>A to Z</button></li>
+			<li><button>Z to A</button></li>
+			<li><button>Newest Contact</button></li>
+			<li><button>Oldest Contact</button></li>
 		</ul>
 	</div>
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,84 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-    ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
-      backgroundImage:{
-        'panel-bg':"url('./assets/panel-bg.jpg')",
-        'select-arrow':"url('./assets/down.png')",
-        'search-icon':"url('./assets/search-icon.png')"
+      backgroundImage: {
+        'panel-bg': "url('./assets/panel-bg.jpg')",
+        'select-arrow': "url('./assets/down.png')",
+        'search-icon': "url('./assets/search-icon.png')",
       },
-      backgroundPosition:{
+      backgroundPosition: {
         'select-arrow': 'right 5px center',
-        'search-icon':'left 8px center'
+        'search-icon': 'left 8px center',
       },
-      backgroundSize:{
-        'select-arrow':'15px 15px',
-        'search-icon':'22px 20px'
+      backgroundSize: {
+        'select-arrow': '15px 15px',
+        'search-icon': '22px 20px',
       },
-      colors:{
-        'primary':'#E904CF',
-        'secondary':'#110554',
-        'high':'#FA2323',
-        'medium-high':'#FAF023',
-        'medium':'#39FF32',
-        'medium-low':'#0555F9',
-        'low':'#E6E6E6'
+      colors: {
+        'primary': '#E904CF',
+        'secondary': '#110554',
+        'high': '#FA2323',
+        'medium-high': '#FAF023',
+        'medium': '#39FF32',
+        'medium-low': '#0555F9',
+        'low': '#E6E6E6',
       },
-      width:{
-        'priority-small':'15px',
-        'priority-medium':'17px',
-        'priority-huge':'25px'
+      width: {
+        'priority-small': '15px',
+        'priority-medium': '17px',
+        'priority-huge': '25px',
       },
-      height:{
-        'priority-small':'15px',
-        'priority-medium':'17px',
-        'priority-huge':'25px'
+      height: {
+        'priority-small': '15px',
+        'priority-medium': '17px',
+        'priority-huge': '25px',
       },
-      boxShadow:{
-        'high':'0 0 10px 0px rgba(250, 35, 35, 0.5)',
-        'medium-high':'0 0 10px 0px rgba(250, 240, 35,0.5)',
-        'medium':'0 0 10px 0px rgba(57, 255, 50,0.5)',
-        'medium-low':'0 0 10px 0px rgba(5, 85, 249,0.5)',
-        'low':'0 0 10px 0px rgba(230, 230, 230,0.5)'
-
+      boxShadow: {
+        'high': '0 0 10px 0px rgba(250, 35, 35, 0.5)',
+        'medium-high': '0 0 10px 0px rgba(250, 240, 35, 0.5)',
+        'medium': '0 0 10px 0px rgba(57, 255, 50, 0.5)',
+        'medium-low': '0 0 10px 0px rgba(5, 85, 249, 0.5)',
+        'low': '0 0 10px 0px rgba(230, 230, 230, 0.5)',
       }
     },
   },
   plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+    plugin(function({ addComponents }) {
+      const sortingBox = {
+        '.sorting-box': {
+          backgroundColor: 'white',
+          padding: '0.5rem',
+          position: 'absolute',
+          zIndex: 2,
+          minWidth: '200px',
+          right: 0,
+          borderRadius: '0.5rem',
+          top: '40px'
+        },
+        '.sorting-box > li': {
+           width:'100%'
+        },
+        '.sorting-box > li > button': {
+          color: '#110554',
+          fontWeight: '300',
+          padding: '0.25rem',
+          width: '100%',
+          textAlign: 'left',
+          borderRadius: '5px',
+          paddingLeft: '1rem',
+        },
+        '.sorting-box > li > button:hover':{
+          backgroundColor:'#110554',
+          color:'white'
+        }
+      };
 
+      addComponents(sortingBox);
+    }),
+    require('tailwind-scrollbar-hide'),
+  ],
+};
